f491063f60744324d9a4f04d6f4926f1e5bdff9f
Moises B <mbaez091@gmail.com>
2023-02-05 18:17:13 -0300

    test refactor

--- tests/user-unit-test.js
+++ tests/user-unit-test.js
@@ -9,21 +9,55 @@ const helpers = require("../helpers");
 
 describe("UserController", function () {
   describe("createUser", function () {
-    it("Should register a user", async function () {
+    let fakeClient,
+      getClientStub,
+      client,
+      releaseStub,
+      queryStub,
+      res,
+      statusStub,
+      sendStub,
+      jsonStub;
+
+    beforeEach(async () => {
       // db client setup
       // TODO: can this be simplified?
 
-      const fakeClient = {
+      fakeClient = {
         release: () => {},
         query: () => {},
       };
 
-      const getClientStub = sinon.stub(db, "getClient").returns(fakeClient);
+      getClientStub = sinon.stub(db, "getClient").returns(fakeClient);
 
-      const client = await db.getClient();
+      client = await db.getClient();
 
-      const releaseStub = sinon.spy(client, "release");
+      releaseStub = sinon.spy(client, "release");
 
+      // response setup
+
+      res = {
+        status: function () {},
+        json: function () {},
+        send: function () {},
+      };
+
+      statusStub = sinon.stub(res, "status").returns(res);
+      jsonStub = sinon.spy(res, "json");
+      sendStub = sinon.spy(res, "send");
+    });
+
+    afterEach(() => {
+      // clean up
+
+      getClientStub.restore();
+      queryStub.restore();
+      statusStub.restore();
+      jsonStub.restore();
+      releaseStub.restore();
+    });
+
+    it("Should register a user", async function () {
       // db data setup
 
       const mockUser = {
@@ -46,7 +80,7 @@ describe("UserController", function () {
         rows: mockDbRows,
       };
 
-      const queryStub = sinon.stub(client, "query").returns(mockQueryResult);
+      queryStub = sinon.stub(client, "query").returns(mockQueryResult);
 
       // password hash spy
 
@@ -58,17 +92,6 @@ describe("UserController", function () {
         body: mockUser,
       };
 
-      // response setup
-
-      const res = {
-        status: function () {},
-        send: function () {},
-      };
-
-      const statusStub = sinon.stub(res, "status").returns(res);
-
-      const sendStub = sinon.spy(res, "send");
-
       // call to createUser
 
       await createUser(req, res);
@@ -77,7 +100,8 @@ describe("UserController", function () {
 
       // expects
 
-      expect(hashPasswordStub.calledOnceWithExactly(mockUser.passwd)).to.be.true;
+      expect(hashPasswordStub.calledOnceWithExactly(mockUser.passwd)).to.be
+        .true;
       expect(queryStub.calledOnce).to.be.true;
       expect(statusStub.calledOnceWithExactly(httpStatus.CREATED)).to.be.true;
       expect(
@@ -86,28 +110,9 @@ describe("UserController", function () {
         })
       ).to.be.true;
       expect(releaseStub.calledOnce).to.be.true;
-
-      // clean up
-
-      getClientStub.restore();
-      queryStub.restore();
     });
 
     it("Should not register a user when username is not provided", async function () {
-      // db client setup
-      // TODO: can this be simplified?
-
-      const fakeClient = {
-        release: () => {},
-        query: () => {},
-      };
-
-      const getClientStub = sinon.stub(db, "getClient").returns(fakeClient);
-
-      const client = await db.getClient();
-
-      const releaseStub = sinon.spy(client, "release");
-
       // db data setup
 
       const mockUser = {
@@ -129,24 +134,13 @@ describe("UserController", function () {
         rows: mockDbRows,
       };
 
-      const queryStub = sinon.stub(client, "query").returns(mockQueryResult);
+      queryStub = sinon.stub(client, "query").returns(mockQueryResult);
 
       // request setup
 
       const req = {
         body: mockUser,
       };
-
-      // response setup
-
-      const res = {
-        status: function () {},
-        send: function () {},
-      };
-
-      const statusStub = sinon.stub(res, "status").returns(res);
-
-      const sendStub = sinon.spy(res, "send");
 
       // call to createUser
 
@@ -165,29 +159,9 @@ describe("UserController", function () {
         })
       ).to.be.true;
       expect(releaseStub.calledOnce).to.be.true;
-
-      // clean up
-
-      getClientStub.restore();
-      queryStub.restore();
-      releaseStub.restore();
     });
 
     it("Should not register a user when password is not provided", async function () {
-      // db client setup
-      // TODO: can this be simplified?
-
-      const fakeClient = {
-        release: () => {},
-        query: () => {},
-      };
-
-      const getClientStub = sinon.stub(db, "getClient").returns(fakeClient);
-
-      const client = await db.getClient();
-
-      const releaseStub = sinon.spy(client, "release");
-
       // db data setup
 
       const mockUser = {
@@ -209,24 +183,13 @@ describe("UserController", function () {
         rows: mockDbRows,
       };
 
-      const queryStub = sinon.stub(client, "query").returns(mockQueryResult);
+      queryStub = sinon.stub(client, "query").returns(mockQueryResult);
 
       // request setup
 
       const req = {
         body: mockUser,
       };
-
-      // response setup
-
-      const res = {
-        status: function () {},
-        send: function () {},
-      };
-
-      const statusStub = sinon.stub(res, "status").returns(res);
-
-      const sendStub = sinon.spy(res, "send");
 
       // call to createUser
 
@@ -243,31 +206,59 @@ describe("UserController", function () {
         })
       ).to.be.true;
       expect(releaseStub.calledOnce).to.be.true;
-
-      // clean up
-
-      getClientStub.restore();
-      queryStub.restore();
-      releaseStub.restore();
     });
   });
 
   describe("getUserById", function () {
-    it("Should get user by id", async function () {
+    let fakeClient,
+      getClientStub,
+      client,
+      releaseStub,
+      queryStub,
+      res,
+      statusStub,
+      sendStub,
+      jsonStub;
+
+    beforeEach(async () => {
       // db client setup
       // TODO: can this be simplified?
 
-      const fakeClient = {
+      fakeClient = {
         release: () => {},
         query: () => {},
       };
 
-      const getClientStub = sinon.stub(db, "getClient").returns(fakeClient);
+      getClientStub = sinon.stub(db, "getClient").returns(fakeClient);
 
-      const client = await db.getClient();
+      client = await db.getClient();
 
-      const releaseStub = sinon.spy(client, "release");
+      releaseStub = sinon.spy(client, "release");
 
+      // response setup
+
+      res = {
+        status: function () {},
+        json: function () {},
+        send: function () {},
+      };
+
+      statusStub = sinon.stub(res, "status").returns(res);
+      jsonStub = sinon.spy(res, "json");
+      sendStub = sinon.spy(res, "send");
+    });
+
+    afterEach(() => {
+      // clean up
+
+      getClientStub.restore();
+      queryStub.restore();
+      statusStub.restore();
+      jsonStub.restore();
+      releaseStub.restore();
+    });
+
+    it("Should get user by id", async function () {
       // user data setup
 
       const idToSearch = 1000;
@@ -286,7 +277,7 @@ describe("UserController", function () {
         rows: mockDbRows,
       };
 
-      const queryStub = sinon.stub(client, "query").returns(mockQueryResult);
+      queryStub = sinon.stub(client, "query").returns(mockQueryResult);
 
       // request setup
 
@@ -296,14 +287,6 @@ describe("UserController", function () {
         },
       };
 
-      // response setup
-
-      const res = { status: function () {}, json: function () {} };
-
-      const statusStub = sinon.stub(res, "status").returns(res);
-
-      const jsonStub = sinon.spy(res, "json");
-
       // get user call
 
       await getUserById(req, res);
@@ -311,7 +294,7 @@ describe("UserController", function () {
       // expects
 
       expect(queryStub.calledOnce).to.be.true;
-      expect(statusStub.calledOnceWithExactly(200)).to.be.true;
+      expect(statusStub.calledOnceWithExactly(httpStatus.OK)).to.be.true;
       expect(
         jsonStub.calledOnceWithExactly({
           data: mockDbRows,
@@ -320,14 +303,6 @@ describe("UserController", function () {
         })
       ).to.be.true;
       expect(releaseStub.calledOnce).to.be.true;
-
-      // clean up
-
-      getClientStub.restore();
-      queryStub.restore();
-      statusStub.restore();
-      jsonStub.restore();
-      releaseStub.restore();
     });
   });
 });
