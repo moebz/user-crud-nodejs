6751dd681165a30b565fba9842db5fec55dbacab
Moises B <mbaez091@gmail.com>
2023-04-16 23:01:24 -0400

    if user is deleted, delete refresh token (cascade in fk)

--- sql/20230303_1_authentication.sql
+++ sql/20230303_1_authentication.sql
@@ -5,4 +5,5 @@ CREATE TABLE public.refresh_token (
 	CONSTRAINT refresh_token_fk FOREIGN KEY (user_account_id) REFERENCES public.user_account(id)
 );
 
-ALTER TABLE public.refresh_token ADD CONSTRAINT refresh_token_un UNIQUE (token_value);
+-- ON DELETE CASCADE: DELETE TOKEN ROW WHEN USER IS DELETED
+ALTER TABLE public.refresh_token ADD CONSTRAINT refresh_token_fk FOREIGN KEY (user_account_id) REFERENCES public.user_account(id) ON DELETE CASCADE;
